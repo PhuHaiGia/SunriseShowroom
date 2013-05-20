95477aec73207eb4f27fc63ecc388c5e709db918
ThanhLCM <thanhlcm90@gmail.com>
2013-05-20 23:21:32 +0700

    Framework and Example full

---
+++ DataAccess/DataBusiness/IShowroomRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Showroom.Models.DataBusiness
+{
+    public interface  IShowroomRepository
+    {
+        void Dispose();
+
+        List<Catelogue> GetProductCatelogueList();
+        Catelogue GetProductCatelogueInfo(int id);
+        bool InsertProductCatelogue(Catelogue item);
+        bool UpdateProductCatelogue(Catelogue item);
+        bool DeleteProductCatelogue(int Id);
+
+    }
+}
--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Showroom.Models.DataAccess;
 
 namespace Frontend.Controllers
 {
     public class HomeController : Controller
     {
+        ShowroomRepository rep = new ShowroomRepository();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -18,6 +21,12 @@ namespace Frontend.Controllers
         public ActionResult About()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            var list = rep.GetProductCatelogueList();
+            return View(list);
         }
     }
 }
