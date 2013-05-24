8cad46e1a772bed85451634dc51bcaa97507972b
Van Duc Nguyen <nguyenduc.ak@gmail.com>
2013-05-25 00:32:35 +0700

    Add controller

---
+++ Backend/Controllers/CatalogueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Showroom.Models.DataAccess;
+namespace SunriseShowroom.Controllers
+{
+    public class CatalogueController : Controller
+    {
+        //
+        // GET: /Catalogue/
+        private ShowroomRepository rep = new ShowroomRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // POST: /Customer/GetList
+        [HttpPost]
+        [Authorize]
+        public ActionResult GetList()
+        {
+            // Action GetList, dùng cho Grid load danh sách dữ liệu
+            var model = rep.GetProductCatalogueList();
+            return View(model);
+        }
+    }
+}
---
+++ DataAccess/Models/CatalogueModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Showroom.Models.Models
+{
+    [MetadataType(typeof(Catalogue.CatalogueMetadata))]
+    public partial class Catalogue
+    {
+        internal sealed class CatalogueMetadata
+        {
+            [Required(ErrorMessage = "Bạn phải nhập tên danh mục.")]
+            [Display(Name = "Tên")]
+            [StringLength(100)]
+            public string Name { get; set; }
+
+            [Display(Name = "Tên không dấu")]
+            [StringLength(255)]
+            public string NameEn { get; set; }
+
+            [Required(ErrorMessage = "Bạn phải chọn logo.")]
+            [Display(Name = "Logo")]
+            public int Image { get; set; }
+
+            [Display(Name = "Độ ưu tiên")]
+            public int? Priority { get; set; }
+
+            [Display(Name = "Trạng thái")]
+            public char Actflg { get; set; }
+        }
+    }
+}
