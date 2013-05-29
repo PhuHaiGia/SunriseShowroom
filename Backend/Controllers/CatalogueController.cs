81e3b31582f472d0edfd35f9586a8dc7e082295d
Van Duc Nguyen <nguyenduc.ak@gmail.com>
2013-05-29 23:10:39 +0700

    Create Catalog Management

--- Backend/Controllers/CatalogueController.cs
+++ Backend/Controllers/CatalogueController.cs
@@ -4,25 +4,51 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Showroom.Models.DataAccess;
+using System.Linq;
+using Showroom.Models;
 namespace SunriseShowroom.Controllers
 {
     public class CatalogueController : Controller
     {
+        private ShowroomRepository rep = new ShowroomRepository();
         //
         // GET: /Catalogue/
-        private ShowroomRepository rep = new ShowroomRepository();
+       
         public ActionResult Index()
         {
-            return View();
-        }
-        // POST: /Customer/GetList
-        [HttpPost]
-        [Authorize]
-        public ActionResult GetList()
-        {
             // Action GetList, dùng cho Grid load danh sách dữ liệu
-            var model = rep.GetProductCatalogueList();
-            return View(model);
+            var CatalogueList = rep.GetProductCatalogueList();
+            return View(CatalogueList);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Catalogue catalogue = rep.GetProductCatalogueInfo(id);
+            return View(catalogue);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Catalogue catalogue)
+        {
+            if (ModelState.IsValid)
+            {
+                catalogue.NameEn = "Nay thi name en";
+                rep.UpdateProductCatalogue(catalogue);
+                return RedirectToAction("Index");
+            }
+            return View(catalogue);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                rep.DeleteProductCatalogue(id);
+                return RedirectToAction("Index");
+            }
+            catch {
+                return RedirectToAction("Index");
+            }            
         }
     }
 }
