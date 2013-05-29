bf5d67483df8279195bb78f39757f5b78200acad
nguyentv91 <wacky.nk@gmail.com>
2013-05-29 23:28:14 +0700

    modify product page

--- Frontend/Controllers/ProductController.cs
+++ Frontend/Controllers/ProductController.cs
@@ -3,20 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Showroom.Models.DataAccess;
 
 namespace Frontend.Controllers
 {
     public class ProductController : Controller
     {
+        ShowroomRepository rep = new ShowroomRepository();
+
         //
         // GET: /Product/
 
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            return View();
+           var model =  rep.GetProductInfo(id);
+            // Lấy danh sách Show main
+           var PropertyList = rep.GetPropertyOFProduct(model.Id);
+           ViewBag.PropertyList = PropertyList;
+
+            // Lấy danh sách chuyên mục
+           var ProCate = rep.GetPropertyCatalogueList();
+           ViewBag.PropertyCatalogueList = ProCate;
+
+            // Lấy danh sách Sub Cate
+           var ProSubCate = rep.GetPropertySubCatalogueList();
+           ViewBag.PropertySubCatalogueList = ProSubCate;
+
+            // Lấy danh sách thuộc tính của product theo ID của sản phẩm
+           var ProductProperty = rep.GetPropertyProductList(id);
+           ViewBag.ProductPropertyList = ProductProperty;
+
+           return View(model);
         }
 
-
+        public ActionResult Product(int id)
+        {
+            var list = rep.GetProductCatalogueList();
+            ViewBag.CatalogueListDrop = (from p in list
+                                         select new SelectListItem
+                                         {
+                                             Value = p.Id.ToString(),
+                                             Text = p.Name,
+                                         }).ToList();
+            var ProductByCatalogue = rep.GetListProductSame(id);
+            ViewBag.ProductByCatalogue = ProductByCatalogue;
+            return View();
+        }
 
     }
 }
