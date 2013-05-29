1d34002da27c9cf9a8ad4c8172b98be270ae9add
nguyentv91 <wacky.nk@gmail.com>
2013-05-29 23:20:40 +0700

    Product file cs

--- DataAccess/DataAccess/Product.cs
+++ DataAccess/DataAccess/Product.cs
@@ -7,7 +7,7 @@ using Showroom.Models.DataBusiness;
 
 namespace Showroom.Models.DataAccess
 {
-    public partial class ShowroomRepository : IShowroomRepository 
+    public partial class ShowroomRepository : IShowroomRepository
     {
         /// <summary>
         /// Lấy toàn bộ danh sách thuộc tính sản phẩm
@@ -25,5 +25,44 @@ namespace Showroom.Models.DataAccess
                 throw ex;
             }
         }
+
+        /// <summary>
+        /// Lấy thông tin của 1  sản phẩm
+        /// </summary>
+        /// <param name="id">Mãsản phẩm</param>
+        /// <returns></returns>
+        public Product GetProductInfo(int id)
+        {
+            try
+            {
+                var item = from p in _dataContext.Products
+                           where p.Id == id
+                           select p;
+                // Trả về 1 giá trị hoặc mặc định (null)
+                return item.SingleOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Product> GetListProductSame(int IdCatalogue)
+        {
+            try
+            {
+                var list = from p in _dataContext.Products
+                           where p.CatelogueId == IdCatalogue 
+                           select p;
+                return list.ToList();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+
     }
 }
